f20063ad58cbd2a5c489fb20a47f854fa0ac15bb
Damianoo00 <56802946+Damianoo00@users.noreply.github.com>
2021-08-11 18:37:37 +0200

    Second screen

--- Python_Api/GUI.py
+++ Python_Api/GUI.py
@@ -1,13 +1,14 @@
-from typing import Text
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class MyGrid(Widget):
+class MainWindow(Screen):
     ip = ObjectProperty(None)
     time_I = ObjectProperty(None)
     time_II = ObjectProperty(None)
@@ -18,12 +19,22 @@ class MyGrid(Widget):
     def btn(self):
         print("ip: ", self.ip.text, "time_I: ", self.time_I.text, "time_II: ", self.time_II.text, "time_III: ", self.time_III.text)
 
+class ProcessWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("watering.kv")
 
 
-class WateringApp(App):
+
+
+
+class WateringMainApp(App):
 
     def build(self):
-        return MyGrid()
+        return kv
 
 if __name__ == '__main__':
-    WateringApp().run()
+    WateringMainApp().run()
